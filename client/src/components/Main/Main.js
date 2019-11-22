bc20856575b8977a97b1d24b331290e5380f3a7e
jason tlh <jasonjdh94@gmail.com>
2019-11-22 15:27:36 -0500

    finished product

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import './Main.scss'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        Main
+      </div>
+    )
+  }
+}
+
+
+
+{/* <div style={{maxWidth: 854 + 'px'}}><div style={{position: 'relative'},{height:0},{paddingBottom:56.25 + '%'}}><iframe src="https://embed.ted.com/talks/tony_robbins_why_we_do_what_we_do" width="854" height="480" style={{position:'absolute'},{left:0},{top:0},{width:100 + '%'},{height:100 + '%'}} frameborder="0" scrolling="no" allowfullscreen></iframe></div></div> */}
+// "https://embed.ted.com/talks/ken_robinson_says_schools_kill_creativity"
+// "https://embed.ted.com/talks/tony_robbins_why_we_do_what_we_do"
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Main from './components/Main';
+import VideoPage from './components/VideoPage';
+
+ReactDOM.render(
+<BrowserRouter>
+  <Switch>
+    <Route path ='/' exact component={App} />
+    {/* <Route path ='/:id' exact component={VideoPage} /> */}
+  </Switch>
+</BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
